9cd981ba7f3057c3c64d23aeb6acf4020d34ceee
Lé <elcasaboxi@gmail.com>
2021-07-28 16:19:55 +0800

    initial commit 📜

---
+++ README.md
@@ -0,0 +1,52 @@
+# Olleatlas
+
+My personal API template. Preview on [Heroku](https://olleatlas.herokuapp.com/users/1). ✌️
+
+## Architecture
+
+Built with Node.js, database on MongoDB Atlas, deployed on Heroku. 💪
+
+Powered by:
+- [node.js](https://github.com/nodejs/node) environment
+- [express](https://github.com/expressjs/express) framework
+- [mongodb](https://github.com/mongodb/node-mongodb-native) for connecting to database hosted on MongoDB Atlas
+- [heroku](https://github.com/heroku/cli) for deployment through CLI
+- [dotenv](https://github.com/motdotla/dotenv) for locally managing environment variables
+
+## Stable versions
+
+- node.js: 16.4.2
+- dotenv: ^10.0.0
+- express: ^4.17.1
+- mongodb: ^3.6.10 (dependency with version 4.0.0 and higher breaks the code!)
+
+## Setup
+
+```bash
+npm install
+node index.js
+```
+
+Note: Any dependency changes will require another `npm install` to be put into effect.
+
+## Deployment
+
+First-time deployment with [Heroku](http://heroku.com/).
+
+```bash
+heroku login
+heroku create app-name
+heroku git:remote -a app-name
+git push heroku main
+```
+
+Following deployments, simply:
+```bash
+git push heroku main
+```
+
+Note: Anytime when deploying with Heroku CLI, commit main branch but don't push before deploying onto Heroku.
+
+---
+
+🍉 Last updated on July 2021 🍉
---
+++ index.js
@@ -0,0 +1,193 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const { MongoClient } = require("mongodb");
+const uri = process.env.DB_URI;
+
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Up on http://localhost:${port}`);
+});
+
+app.use(function (request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+/* --- Routes --- */
+
+app.get("/", (req, res) => {
+  res.send("Olleatlas Sample API");
+});
+
+app.get("/users/:id", (req, res) => {
+  let id = cleanStr(req.params.id);
+  getUser(id).then((data) => {
+    res.send(data);
+  });
+});
+
+/* --- API: GET --- */
+
+function get(collectionName, params, options) {
+  params = params ? params : {};
+  options = options ? options : {};
+  let itemsRetrieved = new Promise(async (resolve, reject) => {
+    let client = new MongoClient(uri);
+    let data;
+
+    try {
+      await client.connect();
+      await client
+        .db("olleatlas")
+        .collection(collectionName)
+        .find(params, { collation: { locale: "en", strength: 2 } }) // Case-insensitive MongoDB query - https://stackoverflow.com/a/40914924
+        .toArray(function (err, result) {
+          if (!isEmpty(result)) {
+            if (options.single) {
+              data = result[0];
+              delete data._id; // MongoDB automatically adds an `_id` prop to its objects; don't show that at the endpoint
+            } else {
+              data = result;
+              data.forEach((d) => delete d._id); // MongoDB automatically adds an `_id` prop to its objects; don't show that at the endpoint
+            }
+          } else {
+            data = { error: "No data." };
+          }
+        });
+    } finally {
+      await client.close();
+      resolve(data);
+    }
+  });
+
+  return itemsRetrieved;
+}
+
+function getUser(id) {
+  return get(
+    "users",
+    {
+      id: id,
+    },
+    {
+      single: true,
+    }
+  );
+}
+
+/* --- API: Add --- */
+
+// app.get("/add/user/:params", (req, res) => {
+//   let params = cleanStr(req.params.params);
+//   addUser(params).then((resCode) => {
+//     res.send(resCode);
+//   });
+// });
+
+app.post("/add/user/:params", (req, res) => {
+  let params = cleanStr(req.params.params);
+  addUser(params).then((resCode) => {
+    res.send(resCode);
+  });
+});
+
+function addUser(params) {
+  let userAdded = add("users", {
+    ...JSON.parse(params),
+    codes: [],
+  });
+  return userAdded;
+}
+
+function add(collectionName, obj) {
+  obj = obj ? obj : {};
+  let itemAdded = new Promise(async (resolve, reject) => {
+    let client = new MongoClient(uri);
+
+    try {
+      await client.connect();
+      await client.db("olleatlas").collection(collectionName).insertOne(obj);
+    } finally {
+      await client.close();
+      resolve(201);
+    }
+  });
+
+  return itemAdded;
+}
+
+/* --- API: Update --- */
+
+// app.get("/update/user/:id/:params", (req, res) => {
+//   let id = cleanStr(req.params.id);
+//   let params = cleanStr(req.params.params);
+//   updateUser(id, params).then((resCode) => {
+//     res.send(resCode);
+//   });
+// });
+
+app.post("/update/user/:id/:params", (req, res) => {
+  let id = cleanStr(req.params.id);
+  let params = cleanStr(req.params.params);
+  updateUser(id, params).then((resCode) => {
+    res.send(resCode);
+  });
+});
+
+function updateUser(id, params) {
+  let userUpdated = update(
+    "users",
+    {
+      id: id,
+    },
+    JSON.parse(params)
+  );
+
+  return userUpdated;
+}
+
+function update(collectionName, idObj, setObj) {
+  idObj = idObj ? idObj : {};
+  setObj = setObj ? setObj : {};
+  let itemUpdated = new Promise(async (resolve, reject) => {
+    let client = MongoClient(uri);
+
+    try {
+      await client.connect();
+      await client.db("olleatlas").collection(collectionName).updateOne(idObj, {
+        $set: setObj,
+      });
+    } finally {
+      await client.close();
+      resolve(200);
+    }
+  });
+
+  return itemUpdated;
+}
+
+/* --- Utils --- */
+
+function cleanStr(str) {
+  return str.toString().trim();
+}
+
+function isEmpty(obj) {
+  for (let i in obj) return false;
+  return true;
+}
+
+function paramsToObj(params) {
+  return Object.fromEntries(
+    params
+      .replace(/%20/g, " ")
+      .split("&")
+      .map((s) => s.split("="))
+  );
+}
